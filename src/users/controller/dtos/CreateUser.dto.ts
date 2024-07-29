5267a0c2bbee879a2ab80e6344d70b97f9b84d33
Tunahan BUCAK <int.tunahan.bucak@aurorabilisim.com>
2024-07-29 13:50:10 +0300

    install middlewares

--- src/users/controller/dtos/CreateUser.dto.ts
+++ src/users/controller/dtos/CreateUser.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, isEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,11 +1,25 @@
-import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+  ParseBoolPipe,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from '../dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getUsers() {
+  getUsers(@Query('sortDesc', ParseBoolPipe) sortDesc: boolean) {
+    console.log(sortDesc);
     return [{ username: 'Tunahan', email: 'tunahan@mail.com' }];
   }
 
@@ -15,15 +29,22 @@ export class UsersController {
   //   response.send('Created');
   // }
 
-  // @Post('create')
-  // createUser(@Body() userData: CreateUserDto) {
-  //   console.log(userData);
-  //   return {};
+  @Post('create')
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
+  }
+
+  // @Get(':id/:postId')
+  // getUserById(@Param('id') id: string, @Param('postId') postId: string) {
+  //   console.log(id, postId);
+  //   return { id, postId };
   // }
 
-  @Get(':id/:postId')
-  getUserById(@Param('id') id: string, @Param('postId') postId: string) {
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
     console.log(id);
-    return { id, postId };
+    return { id };
   }
 }
